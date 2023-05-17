687e19ca7698775a53a5de83c81981b7465f0f32
stevelee477 <hi.whoareyou12@gmail.com>
2023-05-17 20:38:51 +0800

    make cmake great again

--- src/storage/StorageServer.cpp
+++ src/storage/StorageServer.cpp
@@ -1,5 +1,22 @@
 #include "StorageServer.hpp"
 
+Pool::Pool(std::string dev, size_t _size) : size(_size) {
+    dev_fd = open(dev.c_str(), O_RDWR);
+    if (dev_fd < 0) {
+        std::perror("open");
+    }
+    void *mmptr = mmap(NULL, size, PROT_READ | PROT_WRITE, MAP_SHARED, dev_fd, 0);
+    if (mmptr == MAP_FAILED) {
+        std::perror("mmap");
+    }
+    MemoryBaseAddress = reinterpret_cast<uint64_t>(mmptr);
+}
+
+Pool::~Pool() {
+    munmap(reinterpret_cast<void *>(MemoryBaseAddress), size);
+    close(dev_fd);
+}
+
 StorageServer::StorageServer(std::string device, size_t size) {
     /*
         1. read config
